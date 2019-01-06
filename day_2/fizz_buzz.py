054996bb326c948e5a24a010ab91016d818a28dd
adadesions <adadesions@gmail.com>
2019-01-07 02:53:25 +0700

    Day 2

---
+++ day_2/fizz_buzz.py
@@ -0,0 +1,17 @@
+'''
+Game Rules
+if divisible by 3 --> Fizz
+if divisible by 5 --> Buzz 
+if both --> Fizz Buzz 
+
+input: 3
+output: Fizz
+
+input: 15
+output: FizzBuzz
+'''
+
+n = 'ada'
+if n in 'happy new year adadesions':
+    print(n)
+
